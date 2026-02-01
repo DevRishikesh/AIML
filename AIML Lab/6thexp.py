de89660d10f4721c0981836fb5af7e7526965948
Rishikesh Ragav <147292837+DevRishikesh@users.noreply.github.com>
2026-02-01 07:34:10 +0530

    Implement decision tree and random forest classifiers
    Added decision tree and random forest classifiers for analysis.

---
+++ AIML Lab/6thexp.py
@@ -0,0 +1,22 @@
+import pandas
+from sklearn import tree
+from sklearn.tree import DecisionTreeClassifier
+import matplotlib.pyplot as plt
+df = pandas.read_csv("data.csv")
+d = {'UK': 0, 'USA': 1, 'N': 2}
+df['Nationality'] = df['Nationality'].map(d)
+d = {'YES': 1, 'NO': 0}
+df['Go'] = df['Go'].map(d)
+features = ['Age', 'Experience', 'Rank', 'Nationality']
+X = df[features]
+y = df['Go']
+dtree = DecisionTreeClassifier()
+dtree = dtree.fit(X, y)
+tree.plot_tree(dtree, feature_names=features)
+
+
+from sklearn.ensemble import RandomForestClassifier
+rf_clf = RandomForestClassifier(n_estimators=100)
+rf_clf.fit(X_train, y_train)
+print_score(rf_clf, X_train, y_train, X_test, y_test, train=True)
+print_score(rf_clf, X_train, y_train, X_test, y_test, train=False)
